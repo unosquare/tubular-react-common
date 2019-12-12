9299557a770750597bbda94d3d14ec72c8e41027
Alejandro Ocampo <kadosh.ivan@gmail.com>
2019-12-11 23:49:38 -0600

    Basic tests for pagination

--- test/components/tubularComponent.tsx
+++ test/components/tubularComponent.tsx
@@ -6,29 +6,39 @@ import localData from '../fixtures/localData';
 export const TubularComponent = () => {
     const { state, api } = useTubular(columns, localData);
     return (
-        <table>
-            <thead>
-                <tr>
-                    {state.columns
-                        .filter(col => col.Visible)
-                        .map(col => {
-                            return <th key={col.Name}>{col.Label}</th>;
-                        })}
-                </tr>
-            </thead>
-            <tbody>
-                {state.data.map((row, index) => {
-                    return (
-                        <tr key={index}>
-                            {state.columns
-                                .filter(col => col.Visible)
-                                .map(col => {
-                                    return <td key={col.Name}>{row[col.Name]}</td>;
-                                })}
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+        <>
+            <div>
+                <button onClick={() => api.goToPage(state.page + 1)}>Go to next page</button>
+                <button onClick={() => api.goToPage(state.page - 1)}>Go to previous page</button>
+            </div>
+            <table>
+                <thead>
+                    <tr role="rowheader">
+                        {state.columns
+                            .filter(col => col.Visible)
+                            .map(col => {
+                                return <th key={col.Name}>{col.Label}</th>;
+                            })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {state.data.map((row, index) => {
+                        return (
+                            <tr key={index}>
+                                {state.columns
+                                    .filter(col => col.Visible)
+                                    .map(col => {
+                                        return (
+                                            <td role="cell" key={col.Name}>
+                                                {row[col.Name]}
+                                            </td>
+                                        );
+                                    })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
     );
 };
--- test/useTubular.spec.tsx
+++ test/useTubular.spec.tsx
@@ -2,19 +2,78 @@ import '@testing-library/jest-dom/extend-expect';
 // NOTE: jest-dom adds handy assertions to Jest and is recommended, but not required
 
 import * as React from 'react';
-import { render, waitForDomChange } from '@testing-library/react';
+import { render, waitForDomChange, getAllByRole, RenderResult, fireEvent } from '@testing-library/react';
 import { TubularComponent } from './components/tubularComponent';
+
+const getRowBoundaries = (rows: any[]) => {
+    const lastRowCells = getAllByRole(rows[9], 'cell');
+    const firstRowCells = getAllByRole(rows[0], 'cell');
+
+    return {
+        first: firstRowCells,
+        last: lastRowCells,
+    };
+};
 
 describe('useTubular', () => {
     it('should render initial state w/o problem', async () => {
-        const orderId = 'Id';
-        const { queryByText, queryAllByText } = render(<TubularComponent />);
+        const { getByRole } = render(<TubularComponent />);
+        const table = getByRole('table');
+        expect(table).toBeDefined();
+    });
 
-        expect(queryByText(orderId)).toBeDefined();
-        expect(queryByText('OXXO')).toBeNull();
+    describe('Pagination', () => {
+        let sut: RenderResult;
+        beforeEach(async () => {
+            sut = render(<TubularComponent />);
+            await waitForDomChange();
+        });
 
-        await waitForDomChange();
-        const oxxoRows = queryAllByText('OXXO');
-        expect(oxxoRows).toHaveLength(2);
+        it('should show first page by default', async () => {
+            const rows = sut.queryAllByRole('row');
+            expect(rows).toHaveLength(10);
+
+            const rowsBoundaries = getRowBoundaries(rows);
+
+            expect(rowsBoundaries.first[0]).toHaveTextContent('1');
+            expect(rowsBoundaries.first[1]).toHaveTextContent('Microsoft');
+
+            expect(rowsBoundaries.last[0]).toHaveTextContent('10');
+            expect(rowsBoundaries.last[1]).toHaveTextContent('Vesta');
+        });
+
+        describe('Changing page', () => {
+            it('should go to page 2', async () => {
+                const nextPageBtn = sut.getByText('Go to next page');
+
+                fireEvent.click(nextPageBtn);
+                await waitForDomChange();
+
+                const rows = sut.queryAllByRole('row');
+                const rowsBoundaries = getRowBoundaries(rows);
+
+                expect(rowsBoundaries.first[0]).toHaveTextContent('11');
+                expect(rowsBoundaries.first[1]).toHaveTextContent('Microsoft');
+
+                expect(rowsBoundaries.last[0]).toHaveTextContent('20');
+                expect(rowsBoundaries.last[1]).toHaveTextContent('OXXO');
+            });
+
+            it('should go to page 1', async () => {
+                const prevPageBtn = sut.getByText('Go to previous page');
+
+                fireEvent.click(prevPageBtn);
+                await waitForDomChange();
+
+                const rows = sut.queryAllByRole('row');
+                const rowsBoundaries = getRowBoundaries(rows);
+
+                expect(rowsBoundaries.first[0]).toHaveTextContent('1');
+                expect(rowsBoundaries.first[1]).toHaveTextContent('Microsoft');
+
+                expect(rowsBoundaries.last[0]).toHaveTextContent('10');
+                expect(rowsBoundaries.last[1]).toHaveTextContent('Vesta');
+            });
+        });
     });
 });
