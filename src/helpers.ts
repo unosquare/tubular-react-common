c6d33b673cbb2a7c0bd645c7213dcea12a9ec848
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-12-27 21:27:56 -0600

    Add util exportGrid

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,7 @@
 import Transformer, {
+    getCsv,
+    getHtml,
+    ColumnModel,
     GridRequest,
     GridResponse,
     TubularHttpClientAbstract,
@@ -44,4 +47,47 @@ export const generateOnRowClickProxy = (onRowClick: any) => {
             onRowClick(row);
         }
     };
+};
+
+function printDoc(gridResult: [], columns: ColumnModel[], gridName: string) {
+    const tableHtml = getHtml(gridResult, columns);
+
+    const documentToPrint = window.open('about:blank', 'Print', 'location=0,height=500,width=800');
+    documentToPrint.document.write(
+        '<link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap/latest/css/bootstrap.min.css" />',
+    );
+    documentToPrint.document.title = gridName;
+    documentToPrint.document.write('<body onload="window.print();">');
+    documentToPrint.document.write(`<h1>${gridName}</h1>`);
+    documentToPrint.document.write(tableHtml);
+    documentToPrint.document.write('</body>');
+    documentToPrint.document.close();
+}
+
+function exportFile(gridResult: [], columns: ColumnModel[]) {
+    const csvFile = getCsv(gridResult, columns);
+
+    const fileURL = URL.createObjectURL(
+        new Blob([`\uFEFF${csvFile}`], {
+            type: 'text/csv;charset=utf-8;',
+        }),
+    );
+
+    const downloadLink = document.createElement('a');
+
+    downloadLink.setAttribute('href', fileURL);
+    downloadLink.setAttribute('id', 'download');
+    downloadLink.setAttribute('download', 'data.csv');
+    document.body.appendChild(downloadLink);
+
+    downloadLink.click();
+    URL.revokeObjectURL(fileURL);
+}
+
+export const exportGrid = (media: string, gridResult: [], columns: ColumnModel[], gridName: string) => {
+    if (media === 'csv') {
+        exportFile(gridResult, columns);
+    } else {
+        printDoc(gridResult, columns, gridName);
+    }
 };
