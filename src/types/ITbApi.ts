999b59a7b56546a86fd954fa67735c9c952d3390
Alejandro Ocampo <kadosh.ivan@gmail.com>
2020-03-25 18:52:53 -0600

    BREAKING CHANGE: tubular-common upgrade.

--- src/types/ITbApi.ts
+++ src/types/ITbApi.ts
@@ -1,13 +1,13 @@
-import { ColumnModel, FilterWrapper } from 'tubular-common';
+import { ColumnModel, CompareOperators } from 'tubular-common';
 
 export interface ITbApi {
     exportTo: (allRows: boolean, exportFunc: (payload: any[], columns: ColumnModel[]) => void) => void;
     goToPage: (page: number) => void;
-    handleFilterChange: (value: FilterWrapper) => void;
+    handleFilterChange: (filterText: string, filterOperator: CompareOperators, filterArgument?: any[]) => void;
     processRequest: () => void;
     setActiveColumn: (column: ColumnModel) => void;
     setColumns: (columns: ColumnModel[]) => void;
-    setFilter: (value: FilterWrapper) => void;
+    setFilter: (filterText: string, filterOperator: CompareOperators, filterArgument?: any[]) => void;
     sortColumn: (columnName: string, multiSort?: boolean) => void;
     updateItemPerPage: (itemsPerPage: number) => void;
     updateSearchText: (searchText: string) => void;
