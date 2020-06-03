2df2d1d5036a07f1416b45bd76e6def8d1815167
Erick Garcia <erick.garcia@unosquare.com>
2020-06-03 16:57:53 -0500

    Enhanced description

--- test/useMasterDetails.spec.tsx
+++ test/useMasterDetails.spec.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { render, fireEvent, getByText, getByTestId } from '@testing-library/react';
 import { MasterDetailsComponent } from './components/masterDetailsComponent';
 
-describe('useGridRefresh', () => {
-    it('should render initial state w/o problem and refresh value 0', async () => {
+describe('useMasterDetails', () => {
+    it('should render initial state w/o problem and closed', async () => {
         const { container } = render(<MasterDetailsComponent />);
         expect(getByTestId(container, 'openValue').innerHTML).toBe('Closed');
     });
 
-    it('should increase refresh value to 1', async () => {
+    it('should be open', async () => {
         const { container } = render(<MasterDetailsComponent />);
         const openBtn = getByText(container, 'Open/Close');
         fireEvent.click(openBtn);
