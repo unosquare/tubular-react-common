803d8da474956b742c5371241ec17bfd546c21b3
Erick Garcia <erick.garcia@unosquare.com>
2020-06-03 13:08:17 -0500

    sugestions solved

--- test/components/gridRefreshComponent.tsx
+++ test/components/gridRefreshComponent.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { useGridRefresh } from '../../src/useGridRefresh';
-import columns from '../fixtures/columns';
-import localData from '../fixtures/localData';
 
 export const GridRefreshComponent = () => {
     const [refresh, setRefreshCounter] = useGridRefresh();
