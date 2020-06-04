f9f88067007a4c232a48394ffae74934852a0fa4
Erick Garcia <erick.garcia@unosquare.com>
2020-06-04 17:07:10 -0500

    keyDown, keyUp and onClick added

--- test/components/tbTableComponent.tsx
+++ test/components/tbTableComponent.tsx
@@ -2,15 +2,12 @@ import * as React from 'react';
 import { useTbTable } from '../../src/useTbTable';
 import columns from '../fixtures/columns';
 import localData from '../fixtures/localData';
-import { TubularComponent } from './tubularComponent';
 
 export const TbTableComponent = () => {
     const { state, api } = useTbTable(columns, localData);
     return (
         <>
             <div>
-                <button onClick={() => api.goToPage(state.page + 1)}>Go to next page</button>
-                <button onClick={() => api.goToPage(state.page - 1)}>Go to previous page</button>
                 <button onClick={() => api.sortColumn('CustomerName')}>Sort by Customer Name</button>
             </div>
             <table>
--- test/useTbTable.spec.tsx
+++ test/useTbTable.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, getByText } from '@testing-library/react';
 import { TbTableComponent } from './components/tbTableComponent';
 
 describe('TbTableComponent', ()=> {
@@ -7,5 +7,24 @@ describe('TbTableComponent', ()=> {
         const { getByRole } = render(<TbTableComponent />);
         const table = getByRole('table');
         expect(table).toBeDefined();
+    });
+
+    it('Should change setMultiSort value when pressing keyDown and keyUp', async () => {
+        const { container } = render(<TbTableComponent />);
+
+        fireEvent.keyDown(container, { key: 'Control', code: 'Control' });
+        fireEvent.keyUp(container, { key: 'Control', code: 'Control' });
+
+        expect(container).toBeDefined();//Working on a more realistic validation
+    });
+
+    it('Should sort a column', async () => {
+        const { container } = render(<TbTableComponent />);
+
+        fireEvent.keyDown(container, { key: 'Control', code: 'Control' });
+        const sortBtn = getByText(container, 'Sort by Customer Name');
+        fireEvent.click(sortBtn);
+
+        expect(container).toBeDefined();//Working on a more realistic validation
     });
 });
