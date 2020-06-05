9e2a2720330c27f4a826000ff283d0497f4fcfad
Erick Garcia <erick.garcia@unosquare.com>
2020-06-05 12:31:36 -0500

    title validation added

--- test/helpers.spec.ts
+++ test/helpers.spec.ts
@@ -1,4 +1,4 @@
-import { exportGrid, getLocalDataSource, tbId } from '../src/helpers';
+import { exportGrid, getRemoteDataSource, getLocalDataSource, tbId } from '../src/helpers';
 import { simpleRequest } from './mock';
 
 describe('tbId', ()=> {
@@ -11,6 +11,12 @@ describe('tbId', ()=> {
 describe('getLocalDataSource', ()=> {
     it('Rejects', ()=>{
         expect(getLocalDataSource(null)(null)).rejects.not.toBeNull();
+    });
+});
+
+describe('getRemoteDataSource', ()=> {
+    it('Rejects', ()=>{
+        expect(getRemoteDataSource(null)(null)).rejects.not.toBeNull();
     });
 });
 
@@ -27,13 +33,12 @@ describe('exportGrid', ()=> {
         expect(exportGrid('csv', [], simpleRequest.columns, 'Test')).toBeUndefined();
     });
 
-
-    it('Should simulate printDoc', ()=>{
+    it('Should simulate printDoc with title=Test', ()=>{
         const myMock = jest.fn();
         const mockWindow = { document: { write: jest.fn(), close: jest.fn()}};
         myMock.mockReturnValue(mockWindow);
         window.open = myMock;
-
-        expect(exportGrid('', [], simpleRequest.columns, 'Test')).toBeUndefined();
+        exportGrid('', [], simpleRequest.columns, 'Test');
+        expect(mockWindow.document['title']).toBe('Test');
     });
 });
