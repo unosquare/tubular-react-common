0577bd7f016577eeb737c3f2c82a4c73a8d9069d
Erick Garcia <erick.garcia@unosquare.com>
2020-06-05 11:50:10 -0500

    updates

--- test/helpers.spec.ts
+++ test/helpers.spec.ts
@@ -1,5 +1,12 @@
-import { exportGrid, getLocalDataSource } from '../src/helpers';
+import { exportGrid, getLocalDataSource, tbId } from '../src/helpers';
 import { simpleRequest } from './mock';
+
+describe('tbId', ()=> {
+    it('Id should be tbComponent_n+1', ()=>{
+        expect(tbId()).toBe('tbComponent_0');
+        expect(tbId()).toBe('tbComponent_1');
+    });
+});
 
 describe('getLocalDataSource', ()=> {
     it('Rejects', ()=>{
@@ -8,9 +15,25 @@ describe('getLocalDataSource', ()=> {
 });
 
 describe('exportGrid', ()=> {
-    it('init', ()=>{
-        window.open = jest.fn();
+    it('Should simulate exportFile', ()=>{
+        URL.createObjectURL = jest.fn();
+        URL.revokeObjectURL = jest.fn();
 
-        expect(exportGrid('', [], simpleRequest.columns, '')).toThrowError();
+        expect(exportGrid('csv', [], simpleRequest.columns, 'Test')).toBeUndefined();
+    });
+
+    it('Should simulate exportFile with IE 10+', ()=>{
+        navigator.msSaveBlob = jest.fn();
+        expect(exportGrid('csv', [], simpleRequest.columns, 'Test')).toBeUndefined();
+    });
+
+
+    it('Should simulate printDoc', ()=>{
+        const myMock = jest.fn();
+        const mockWindow = { document: { write: jest.fn(), close: jest.fn()}};
+        myMock.mockReturnValue(mockWindow);
+        window.open = myMock;
+
+        expect(exportGrid('', [], simpleRequest.columns, 'Test')).toBeUndefined();
     });
 });
