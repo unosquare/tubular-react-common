d989ca16315b398d725dc1a198c3ad5292a14a3a
Erick Garcia <36867256+ErickGarciaRios@users.noreply.github.com>
2020-06-08 10:15:18 -0500

    Test Helpers (#161)
    * Test Helpers started
    
    * helpers
    
    * updates
    
    * title validation added
    
    * mock.calls.lenght validation added
    
    Co-authored-by: Erick Garcia <erick.garcia@unosquare.com>

---
+++ test/helpers.spec.ts
@@ -0,0 +1,55 @@
+import { exportGrid, getRemoteDataSource, getLocalDataSource, tbId } from '../src/helpers';
+import { simpleRequest } from './mock';
+
+describe('tbId', ()=> {
+    it('Id should be tbComponent_n+1', ()=>{
+        expect(tbId()).toBe('tbComponent_0');
+        expect(tbId()).toBe('tbComponent_1');
+    });
+});
+
+describe('getLocalDataSource', ()=> {
+    it('Rejects', ()=>{
+        expect(getLocalDataSource(null)(null)).rejects.not.toBeNull();
+    });
+});
+
+describe('getRemoteDataSource', ()=> {
+    it('Rejects', ()=>{
+        expect(getRemoteDataSource(null)(null)).rejects.not.toBeNull();
+    });
+});
+
+describe('exportGrid', ()=> {
+    it('Should simulate exportFile', ()=>{
+        const mockCreateObjectURL  = jest.fn();
+        const mockRevokeObjectURL = jest.fn();
+
+        URL.createObjectURL = mockCreateObjectURL;
+        URL.revokeObjectURL = mockRevokeObjectURL;
+
+        exportGrid('csv', [], simpleRequest.columns, 'Test');
+
+        expect(mockCreateObjectURL.mock.calls.length).toBe(1);
+        expect(mockRevokeObjectURL.mock.calls.length).toBe(1);
+    });
+
+    it('Should simulate exportFile with IE 10+', ()=>{
+        const mockMsSaveBlob = jest.fn();
+        navigator.msSaveBlob = mockMsSaveBlob;
+
+        exportGrid('csv', [], simpleRequest.columns, 'Test');
+
+        expect(mockMsSaveBlob.mock.calls.length).toBe(1);
+    });
+
+    it('Should simulate printDoc with title=Test', ()=>{
+        const myMock = jest.fn();
+        const mockWindow = { document: { write: jest.fn(), close: jest.fn()}};
+        myMock.mockReturnValue(mockWindow);
+        window.open = myMock;
+
+        exportGrid('', [], simpleRequest.columns, 'Test');
+        expect(myMock.mock.calls.length).toBe(1);
+    });
+});
