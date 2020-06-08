fef30a2e0b15862576ed4589e39c7a2ea8c978c3
Erick Garcia <erick.garcia@unosquare.com>
2020-06-08 15:08:44 -0500

    updates

--- test/components/tbListComponent.tsx
+++ test/components/tbListComponent.tsx
@@ -7,6 +7,36 @@ export const TbListComponent = () => {
     const { state, api } = useTbList(columns, localData);
     return (
         <>
+            <div>
+                <button onClick={() => api.loadPage(1)}>Go to page 2</button>
+                <button onClick={() => api.search('Microsoft')}>Search</button>
+                <button onClick={() => api.sortByColumn('CustomerName')}>Sort by Customer Name</button>
+            </div>
+            <table>
+                <thead>
+                    <tr role="rowheader">
+                        {state.columns
+                            .filter(col => col.visible)
+                            .map(col =>
+                                <th key={col.name}>{col.label}</th>
+                            )}
+                    </tr>
+                </thead>
+                <tbody>
+                    {state.data.map((row, index) => (
+                            <tr key={index}>
+                                {state.columns
+                                    .filter(col => col.visible)
+                                    .map(col => (
+                                            <td role="cell" key={col.name}>
+                                                {row[col.name]}
+                                            </td>
+                                        )
+                                    )}
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
         </>
     );
 };
