5d95718be04f718c7eed6bb83e591c01ddb416c8
Erick Garcia <36867256+ErickGarciaRios@users.noreply.github.com>
2020-06-09 13:51:55 -0500

    General coverage improvement  (#169)
    * started
    
    * generateOnRowClickProxy test added
    
    Co-authored-by: Erick Garcia <erick.garcia@unosquare.com>

--- test/helpers.spec.ts
+++ test/helpers.spec.ts
@@ -1,4 +1,5 @@
-import { exportGrid, getRemoteDataSource, getLocalDataSource, tbId } from '../src/helpers';
+import { getRemoteDataSource, getLocalDataSource, tbId } from '../src/helpers';
+import { exportGrid, generateOnRowClickProxy } from '../src';
 import { simpleRequest } from './mock';
 
 describe('tbId', ()=> {
@@ -17,6 +18,15 @@ describe('getLocalDataSource', ()=> {
 describe('getRemoteDataSource', ()=> {
     it('Rejects', ()=>{
         expect(getRemoteDataSource(null)(null)).rejects.not.toBeNull();
+    });
+});
+
+describe('generateOnRowClickProxy', ()=> {
+    it('Should execute onRowClick function', ()=>{
+        const row: Record<string, any> = ["key", null];
+        const onRowClick = jest.fn();
+        generateOnRowClickProxy(onRowClick(row));
+        expect(onRowClick.mock.calls.length).toBe(1);
     });
 });
 
