3ccb3065825ba6cacdeb45fd36de152a05849696
Alejandro Ocampo <kadosh.ivan@gmail.com>
2021-02-09 14:25:10 -0600

    Adding common types

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './useTbList';
 export * from './useTbTable';
 export * from './useGridRefresh';
 export * from './useMasterDetails';
+export * from './useTbSelection';
 export * from './types';
 export { generateOnRowClickProxy, exportGrid } from './helpers';
