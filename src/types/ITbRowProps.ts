dc94d05f74c458e882e041dfb83e5d08afeb8b7c
Alejandro Ocampo <kadosh.ivan@gmail.com>
2021-02-09 14:25:10 -0600

    Adding common types

---
+++ src/types/ITbRowProps.ts
@@ -0,0 +1,11 @@
+import { ColumnModel } from "tubular-common";
+import { ITbSelection } from "./ITbSelection";
+
+export interface ITbRowProps {
+    row: any;
+    rowIndex: number;
+    columns: ColumnModel[];
+    onRowClick?(): void;
+    rowSelectionEnabled?: boolean;
+    selection?: ITbSelection;
+}
